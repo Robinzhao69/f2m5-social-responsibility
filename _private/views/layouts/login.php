9ad62e5a390338971d3be4db28c4b111c1a925a4
Robin Zhao <titanen.1987@hotmail.com>
2021-09-24 13:42:55 +0200

    kleine fix voor live server

--- _private/views/layouts/login.php
+++ _private/views/layouts/login.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="<?php echo site_url( '/css/login.css' ) ?>" media="all">
+	<?php if ( $this->section( 'css' ) ): ?>
+		<?php echo $this->section( 'css' ) ?>
+	<?php endif; ?>
 </head>
 <body>
     <div class="registratie">
